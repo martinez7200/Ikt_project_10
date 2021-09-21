58eef610cacc9b3f5438b48eb6ec8ea0877e7336
mi <90605588+martinez7200@users.noreply.github.com>
2021-09-21 18:55:00 +0200

    Add files via upload

---
+++ recept_2.py
@@ -0,0 +1,7 @@
+def Piritott_hamburger_buci(innivaló):
+    print('Veszek két filézzett csirke mellet, megfűszerezem és grillrácson aranybarnára sütöm mind a két oldalán.')
+    print('Azután belerakom buciba a salátát majd az uborkát, felkarikázott lilahagymát.')
+    print('Utána paradicsom, a grillezett csirke szeletet belerakjuk, rá az edami sajtot.')
+    print('És végül leöntjük csípős szósszal ezután rátesszük a buci tetejét.')
+    print('Majd feltálaljuk ' + innivaló + 'vel')
+Piritott_hamburger_buci('Pepsi')
