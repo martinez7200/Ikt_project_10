ee36d90e675cc9338710c9b914e875d8938064d6
mi <90605588+martinez7200@users.noreply.github.com>
2021-10-05 19:18:42 +0200

    Add files via upload

---
+++ hazi.py.py
@@ -0,0 +1,6 @@
+print('Üdv')
+nev = str(input('Hogy hívják? ')) 
+kor =int(input('Mennyi idős? '))
+iskola = str(input('Hol tanul? '))
+atlag = float(input('Mennyi az átlaga? '))
+print('A válaszait köszönjük szépen!')
