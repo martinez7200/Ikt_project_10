5dbfae3328a5616168a26d428e23d15345647697
mi <90605588+martinez7200@users.noreply.github.com>
2021-10-18 07:45:38 +0200

    Add files via upload

---
+++ nagyobb.py
@@ -0,0 +1,8 @@
+a = int(input('Adj meg egy számot! '))
+b = int(input('Adj meg egy másik számot! '))
+if a>b:
+	print('A nagyobb érték a ',a)
+elif b>a:
+	print('A nagyobb érték a  ',b)
+else :
+	print('A két szám egyenlő')
