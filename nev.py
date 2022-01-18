a4d1deb4bb3a91081582eef8eab40e16adc916cc
mi <90605588+martinez7200@users.noreply.github.com>
2022-01-18 14:29:06 +0100

    Add files via upload

---
+++ nev.py
@@ -0,0 +1,13 @@
+while True:
+ nev = str(input("Add meg a neved! "))
+ if nev == "alex":
+     print("Szia alex! ")
+     break
+ elif nev == "martin":
+     print("Szia martin! ")
+     break
+ elif nev == "sanyi":
+     print("Szia sanyi! ")
+     break
+ else:
+     print("Nem jó!")
